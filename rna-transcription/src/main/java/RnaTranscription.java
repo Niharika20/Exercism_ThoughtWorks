9a462d06518cdc7c17e37b94fdd8756c7c3122c9
Niharika <nraheja1995@gmail.com>
2019-07-05 19:20:47 +0530

    Pathshala specific updated

--- rna-transcription/src/main/java/RnaTranscription.java
+++ rna-transcription/src/main/java/RnaTranscription.java
@@ -1,35 +1,68 @@
+import java.util.HashMap;
+
+//updated program using hashmap.
 class RnaTranscription {
+	
+	private HashMap<Character, Character> hm = new HashMap<>();
+	
+	RnaTranscription()
+	{
+		hm.put('G','C');
+		hm.put('C','G');
+		hm.put('T','A');
+		hm.put('A','U');
+	}
+
+    String transcribe(String dnaStrand) {
+        //create an empty string builder object
+		StringBuilder rnaStrand = new StringBuilder();
+		
+		for(int i=0; i<dnaStrand.length(); i++)
+		{
+			//using append function to add the characters in rnaStrand
+			rnaStrand.append(hm.get(dnaStrand.charAt(i)));
+		}
+		
+		//return rnaStrand as String.
+		return rnaStrand.toString();
+	}	
+}
+
+//Also implemented using Switch before.
+
+/*class RnaTranscription {
 
     String transcribe(String dnaStrand) {
         
-		String rnaStrand="";
+		StringBuilder rnaStrand = new StringBuilder();
 		
 		for(int i=0; i<dnaStrand.length(); i++)
 		{
 			switch(dnaStrand.charAt(i))
 			{
 				case 'G':
-				rnaStrand=rnaStrand + "C";
+				rnaStrand.append("C");
 				break;
 				
 				case 'C':
-				rnaStrand=rnaStrand + "G";
+				rnaStrand.append("G");
 				break;
 				
 				case 'T':
-				rnaStrand=rnaStrand + "A";
+				rnaStrand.append("A");
 				break;
 				
 				case 'A':
-				rnaStrand=rnaStrand + "U";
+				rnaStrand.append("U");
 				break;
 			}
 		
 		
 		}
 		
-		return rnaStrand;
+		return rnaStrand.toString();
 
     }
 
 }
+*/
